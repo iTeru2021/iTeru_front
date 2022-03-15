25b3e40973a438fae50bd6b5ed0def5e50bdb6a6
Sugasawa01 <steady.co5@gmail.com>
2022-03-15 16:08:40 +0900

    first commit

---
+++ src/components/modules/api.ts
@@ -0,0 +1,49 @@
+import { RoomState } from "../constants";
+
+// API Rooms
+interface APIRoom {
+  roomName: string;
+  isUsing: boolean;
+  timestamp: string;
+  ppm: string;
+}
+
+const translateAPIRoom = (apiResult: APIRoom[]): RoomState[] => {
+  const rooms: RoomState[] = [];
+
+  apiResult.map(item => {
+    const date = new Date(item.timestamp);
+    const elapsedMinutesFromNow = Math.floor(
+      (Date.now() - date.getTime()) / 60000
+    ); // = ミリ秒 / 1000ミリ秒 / 60秒
+
+    rooms.push({
+      roomId: item.roomName,
+      roomName: item.roomName,
+      isUsing: item.isUsing,
+      co2: item.co2,
+      elapsedMinutes: elapsedMinutesFromNow
+    });
+
+    return item;
+  });
+
+  return rooms;
+};
+
+export const fetchRooms = (locationName: string): Promise<RoomState[]> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      "https://lnezdu8n2l.execute-api.ap-northeast-1.amazonaws.com/rooms/新宿"
+    )
+      .then(response => response.json())
+      .then((json: APIRoom[]) => {
+        console.log(json);
+        resolve(translateAPIRoom(json));
+      })
+      .catch((reason: string) => {
+        console.log(reason);
+        reject(reason);
+      });
+  });
+};
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export interface RoomState {
+  roomId: string;
+  roomName: string;
+  isUsing: boolean;
+  ppm: string;
+  elapsedMinutes: number;
+}
